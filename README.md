f9942a0725e5d4094cd590229674f89258f9f28c
Werthis <lukaszstawecki@gmail.com>
2024-10-09 18:09:26 +0200

    Add status filter

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Rick-and-Morty
+
+Table with Rick and Morty characters
+It uses public API https://rickandmortyapi.com/documentation
--- app.js
+++ app.js
@@ -2,77 +2,90 @@ document.addEventListener("DOMContentLoaded", () => {
   const apiUrl = "https://rickandmortyapi.com/api/character";
   const tableBody = document.getElementById("tableBody");
   const paginationControls = document.getElementById("paginationControls");
+  const statusFilter = document.getElementById("statusFilter");
   let currentPage = 1;
+  let currentStatus = "all";
+  statusFilter.value = "all";
 
-  // Fetch data from API
-  const fetchData = async (page = 1) => {
-    const response = await fetch(`${apiUrl}?page=${page}`);
+  const fetchData = async (page = 1, status) => {
+    let url = `${apiUrl}?page=${page}`;
+    if (status !== "all") {
+      url += `&status=${status}`;
+    }
+    const response = await fetch(url);
     const data = await response.json();
     return data;
   };
 
-  // Render table rows
   const renderTableRows = (characters) => {
     tableBody.innerHTML = "";
     characters.forEach((character) => {
       const row = document.createElement("tr");
-      // row.className = 'flex flex-col w-full'
       row.innerHTML = `
       <td class="w-20 h-20 py-2 px-3 border-b border-r border-gray-300">
         <img class="rounded-full" src="${character.image}" alt="${character.name}">
       </td>
       <td class="sm:w-96 py-2 px-3 border-b border-r border-gray-300">${character.name}</td>
-      <td class="w-20 py-2 px-2 border-b border-gray-300">${character.status}</td>
+      <td class="w-[82px] py-2 px-2 border-b border-gray-300 text-center">${character.status}</td>
     `;
       tableBody.appendChild(row);
     });
   };
 
-  // Create pagination controls
   const renderPagination = (info) => {
-    paginationControls.innerHTML = ""; // Clear previous controls
     const totalPages = info.pages;
+    paginationControls.innerHTML = "";
 
-    for (let i = 1; i <= totalPages; i++) {
+    const isPageVisible = (page) => {
+      return (
+        page === 1 || page === totalPages || Math.abs(page - currentPage) <= 2
+      );
+    };
+
+    const createPageButton = (page) => {
       const pageButton = document.createElement("button");
-      const isBorderElement = i === 1 || i === totalPages;
-      const buttonVisible =
-        i === 1 ||
-        i === currentPage - 2 ||
-        i === currentPage - 1 ||
-        i === currentPage ||
-        i === currentPage + 1 ||
-        i === currentPage + 2 ||
-        i === totalPages;
+      const isCurrentPage = page === currentPage;
+      const isEllipsis =
+        (page === currentPage - 2 || page === currentPage + 2) &&
+        page !== 1 &&
+        page !== totalPages;
 
       pageButton.className = `mx-1 px-3 py-1 rounded ${
-        i === currentPage ? "bg-blue-500 text-white" : "bg-gray-200"
-      } ${buttonVisible ? "" : "hidden"}`;
+        isCurrentPage ? "bg-blue-500 text-white" : "bg-gray-200"
+      } ${isPageVisible(page) ? "" : "hidden"}`;
 
-      pageButton.innerText =
-        (i === currentPage - 2 || i === currentPage + 2) &&
-        !isBorderElement
-          ? "..."
-          : i;
+      pageButton.innerText = isEllipsis ? "..." : page;
 
-      // Add event listener for pagination click
       pageButton.addEventListener("click", () => {
-        currentPage = i;
-        loadPage(currentPage);
+        currentPage = page;
+        loadPage(currentPage, currentStatus);
       });
 
-      paginationControls.appendChild(pageButton);
+      return pageButton;
+    };
+
+    // Use DocumentFragment to minimize reflows
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 1; i <= totalPages; i++) {
+      const pageButton = createPageButton(i);
+      fragment.appendChild(pageButton);
     }
+
+    paginationControls.appendChild(fragment);
   };
 
-  // Load a specific page
-  const loadPage = async (page) => {
-    const data = await fetchData(page);
-    console.log(data);
+  const loadPage = async (page, status) => {
+    const data = await fetchData(page, status);
     renderTableRows(data.results);
     renderPagination(data.info);
   };
 
-  // Initialize the table and pagination
-  loadPage(currentPage);
+  statusFilter.addEventListener("change", (event) => {
+    currentStatus = event.target.value;
+    currentPage = 1;
+    loadPage(currentPage, currentStatus);
+  });
+
+  loadPage(currentPage, currentStatus);
 });
